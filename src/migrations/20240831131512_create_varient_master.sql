61e42d0e2f0374d4bd3d8da7289ae2b25235e345
Nabanita Ghosh <nabanita.ghosh@indware.com>
2024-10-16 12:16:29 +0530

    migration file changes

--- src/migrations/20240831131512_create_varient_master.sql
+++ src/migrations/20240831131512_create_varient_master.sql
@@ -1,11 +1,15 @@
-CREATE TABLE variant_master (
-    id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-    variantName VARCHAR(50) UNIQUE NOT NULL,
-    description TEXT,
+
+CREATE TABLE IF NOT EXISTS public.variant_master
+(
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    "variantName" character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    description text COLLATE pg_catalog."default",
     "createdAt" timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "createdBy" uuid NOT NULL,
     "updatedAt" timestamp without time zone,
     "updatedBy" uuid,
     "isActive" boolean NOT NULL DEFAULT true,
-    "softDelete" boolean NOT NULL DEFAULT false
-);
+    "softDelete" boolean NOT NULL DEFAULT false,
+    CONSTRAINT variant_master_pkey PRIMARY KEY (id),
+    CONSTRAINT variant_master_variantname_key UNIQUE ("variantName")
+)
